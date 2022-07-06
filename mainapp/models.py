7eec0638b81c87c5c26ae273de8a8d7eedf12b09
User <user@Users-MacBook-Pro.local>
2022-07-06 14:08:10 +0600

    create user model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    balance = models.IntegerField(default=0)
+    
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+    
+    def __str__(self):
+        return f"{self.username}"
+
