b7bbc571b0f9e430f6f4720ebe42fdfbc9e1c09a
User <user@Users-MacBook-Pro.local>
2022-07-06 14:19:40 +0600

    create point model and del migrations

--- core/settings.py
+++ core/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
 # }
 
 
-# AUTH_USER_MODEL = 'mainapp.User'
+AUTH_USER_MODEL = 'mainapp.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- mainapp/models.py
+++ mainapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    balance = models.IntegerField(default=0)
+    balance = models.IntegerField(default=0, verbose_name="Баланс")
     
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
@@ -11,3 +11,18 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.username}"
 
+
+class Point(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, 
+        primary_key=True, related_name='user_point', 
+        verbose_name="Пользователь", 
+        )
+    click_count = models.SmallIntegerField(default=1, verbose_name="Счет нажатий")
+    last_click_date = models.DateField(auto_now=True, verbose_name="Дата нажатия")
+    
+    class Meta:
+        db_table = "point"
+        verbose_name = "Балл"
+        verbose_name_plural = "Баллы"
+        
